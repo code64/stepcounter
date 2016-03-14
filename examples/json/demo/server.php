6571013d0bc8cd713c8832cea4f627624febdbec
hofmannsven <info@hofmannsven.com>
2016-03-14 23:42:11 +0100

    Parse event stream as JSON

--- examples/json/demo/server.php
+++ examples/json/demo/server.php
@@ -1,16 +1,14 @@
 <?php
 
-# Set headers for event stream
+# Set headers for event stream.
 header( 'Content-Type: text/event-stream' );
 header( 'Cache-Control: no-cache' );
 
-# Open JSON file
+# Open original JSON file.
 $json = file_get_contents( '../logs/data.json' );
 
-# Decode JSON and re-parse for JSON Server-Sent Events
-$json_array = json_decode( $json, true );
-foreach ( $json_array as $key => $value ) {
-    echo "$key: $value\n\n";
-}
+# Prepare content for event stream.
+echo 'data: ' . $json . "\n\n";
 
+# Flush system output buffer.
 flush();
